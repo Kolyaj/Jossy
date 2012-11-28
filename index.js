0bf4ef0f34d80bf5773779ea9ebb5922217e210a
Kolyaj <kolyaj@yandex.ru>
2012-11-28 14:40:42 +0400

    Сборка из командной строки

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 if (require.main == module) {
-    require('./jossy').compile(process.argv[2], [], {}, function(err, result) {
-        if (err) {
-            throw err;
+    if (process.argv[2]) {
+        var params = process.argv[2].split('::');
+        var context = {};
+        for (var i = 3; i < process.argv.length; i++) {
+            context[process.argv[i]] = true;
         }
-        console.log(result);
-    });
+        require('./jossy').compile(params.shift() || 'index.js', params, context, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+        });
+    } else {
+        console.log('Usage:');
+    }
 } else {
     module.exports = require('./jossy');
 }
