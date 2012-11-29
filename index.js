60f0a1d3042497902041cd9d0cd20efdb99ccf82
Kolyaj <kolyaj@yandex.ru>
2012-11-29 19:46:00 +0400

    Порефакторил index.js

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
 if (require.main == module) {
     if (process.argv[2]) {
-        var context = {};
-
         if (process.argv[2] == 'server') {
-            var startContextArg = 3;
+            var contextVars = process.argv.slice(3);
             var port = 9595;
-            if (+process.argv[3]) {
-                port = +process.argv[3];
-                startContextArg = 4;
+            if (+contextVars[0]) {
+                port = +contextVars.shift();
             }
-            for (; startContextArg < process.argv.length; startContextArg++) {
-                context[process.argv[startContextArg]] = true;
-            }
-            require('http').createServer(function(req, res) {
-                var fname = require('url').parse(req.url).pathname;
-                require('./jossy').compile(fname, [], context, function(err, result) {
-                    if (err) {
-                        console.log('Error: ' + err.message);
-                        res.writeHead(500);
-                        res.end('throw new Error(' + JSON.stringify('JossyError: ' + err.message) + ');');
-                        return;
-                    }
-                    res.writeHead(200, {
-                        'Content-Type': 'text/javascript; charset=UTF-8'
-                    });
-                    res.end(result);
-                });
-            }).listen(port);
+            createServer(port, contextVars);
         } else {
-            var params = process.argv[2].split('::');
-            for (var i = 3; i < process.argv.length; i++) {
-                context[process.argv[i]] = true;
-            }
-            require('./jossy').compile(params.shift() || 'index.js', params, context, function(err, result) {
-                if (err) {
-                    throw err;
-                }
-                console.log(result);
-            });
+            compileFile(process.argv[2], process.argv.slice(3));
         }
     } else {
         console.log('Usage:');
     }
 } else {
     module.exports = require('./jossy');
+}
+
+function compileFile(fileParam, contextVars) {
+    var fileParams = fileParam.split('::');
+    require('./jossy').compile(fileParams.shift() || 'index.js', fileParams, makeContext(contextVars), function(err, result) {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+    });
+}
+
+function createServer(port, contextVars) {
+    require('http').createServer(function(req, res) {
+        var fname = require('url').parse(req.url).pathname;
+        require('./jossy').compile(fname, [], makeContext(contextVars), function(err, result) {
+            if (err) {
+                console.log('Error: ' + err.message);
+                res.writeHead(500);
+                res.end('throw new Error(' + JSON.stringify('JossyError: ' + err.message) + ');');
+                return;
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/javascript; charset=UTF-8'
+            });
+            res.end(result);
+        });
+    }).listen(port);
+}
+
+function makeContext(vars) {
+    var context = {};
+    for (var i = 0; i < vars.length; i++) {
+        context[vars[i]] = true;
+    }
+    return context;
 }
